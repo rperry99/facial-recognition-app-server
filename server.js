a2f62fe75a7985926f5f25c2cea53fdc3eaa8a87
Russ Perry <rperry1886@gmail.com>
2019-05-10 22:47:12 -0400

    Remove register from server.js and put it in its own file

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const app = express();
 const knex = require("knex");
 
+const register = require("./controllers/register");
+
 const db = knex({
   client: "pg",
   connection: {
@@ -62,31 +64,7 @@ app.get("/profile/:id", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-  db.transaction(trx => {
-    trx
-      .insert({
-        hash: hash,
-        email: email
-      })
-      .into("login")
-      .returning("email")
-      .then(loginEmail => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch(err => res.status(400).json("Something went wrong here..."));
+  register.handleRegister(req, res, db, bcrypt);
 });
 
 app.put("/image", (req, res) => {
